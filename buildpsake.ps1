d6ce2f1baaf9569a4342d07f6cb7e69e0a1317c3
Mateusz Mikolajski <mateusz.mikolajski@gmail.com>
2015-11-17 23:07:20 +0100

    added changes to the build script

--- buildpsake.ps1
+++ buildpsake.ps1
@@ -2,16 +2,21 @@ properties {
   $testMessage = 'Executed Test!'
   $compileMessage = 'Executed Compile!'
   $cleanMessage = 'Executed Clean!'
+  $configuration = "Debug"
+  $framework = "4.5"
 }
 
 task default -depends Test
 
 task Test -depends Compile, Clean { 
-  $testMessage
+  exec { 
+        & "src\LazyService\packages\NUnit.Console.3.0.0\tools\nunit3-console.exe" "src\LazyService\LazyService.Tests\bin\Debug\LazyService.Tests.dll" --teamcity --noheader
+    }
+
 }
 
 task Compile -depends Clean { 
-  $compileMessage
+  Exec { msbuild "src/LazyService/LazyService.sln" }
 }
 
 task Clean { 
