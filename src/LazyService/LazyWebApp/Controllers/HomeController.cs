f38f2522b447b2ce25c50cc2e9c98c99ecba8cb3
Mateusz Mikolajski <mateusz.mikolajski@gmail.com>
2015-11-17 23:12:29 +0100

    adding enabling nuget package restore + possibility tests

---
+++ src/LazyService/LazyWebApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LazyWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ConfigurationSource _configurationSource;
+        //
+        // GET: /Home/
+
+        public HomeController()
+        {
+            _configurationSource = new ConfigurationSource();
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = _configurationSource.ServiceName;
+            return View(ViewBag);
+        }
+
+    }
+
+    public class ConfigurationSource
+    {
+        public ConfigurationSource()
+        {
+            ServiceName = ConfigurationManager.AppSettings["WebAppName"];
+        }
+
+        public string ServiceName { get; set; }
+    }
+}
